e161249fe93378bd03e554537b66e39de2c65e6d
rajeshsaharan <16072000rajesh@gmail.com>
2022-07-21 15:42:14 +0530

    nodejs

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 const app = require("./app")
 
 console.log(app.a)
-
+const color = require('colors')
+// const chalk = require('chalk')
 const http = require('http')
-console.log(http)
+// console.log(http)
+
+// console.log(chalk.green('Hello %s'), "hello")
+console.log('hello' .blue);
