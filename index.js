c917452dbad4b7e19aae2c939ac8bf43364ea734
rajeshsaharan <16072000rajesh@gmail.com>
2022-07-22 15:19:28 +0530

    route-middleware

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 console.log("middleware details")
+const { request } = require('express')
+const express = require('express')
+const app = express()
+// application level middleware for all routes
+
+const requestFilter = (request, response, next)=>{
+    //next will procced next
+   if(!request.query.age){
+       response.send("get out fucking here")
+
+   }else if(request.query.age < 18){
+       response.send("please old more")
+   }else{
+       next()
+   }
+}
+
+
+ 
+
+//how to use requestFilter 
+app.use(requestFilter)
+
+
+app.get('', (request, response)=>{
+    response.send("data by response body in / route")
+})
+
+
+
+app.get('/about', (request, response)=>{
+    response.send("data by response body in /about route")
+})
+
+
+app.get('/contact', (request, response)=>{
+    response.send("data by response body in /contact route")
+})
+
+
+
+app.listen(5000)
